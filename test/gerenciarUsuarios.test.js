54b30a24323cd8680e16a6fde0b5c78782d433e2
Silvana <silpiressilva@gmail.com>
2025-11-17 22:04:47 -0300

    Novas funções.

--- test/gerenciarUsuarios.test.js
+++ test/gerenciarUsuarios.test.js
@@ -1,22 +1,11 @@
 const { gerenciarUsuarios, adicionarNovoUsuario } = require('../src/gerenciarUsuarios');
 const { expect } = require('chai');
 
-describe('Testar as funções de usuários', function() {
-
-    it('Deve retornar a lista inicial de usuários', function() {
-        const usuarios = gerenciarUsuarios();
-        expect(usuarios).to.deep.equal(['Théo', 'Silvana', 'Deivede']);
-    });     
+describe('Testar as funções de usuários', function() { 
 
     it('Deve adicionar um novo usuário à lista', function() {
         adicionarNovoUsuario('Maria');
-        const usuarios = gerenciarUsuarios();
-        expect(usuarios).to.include('Maria');
+        const listaUsuarios = gerenciarUsuarios();
+        expect(listaUsuarios.at(-1)).to.equal('Maria');
     });
-
-    it('Deve validar se o novo usuário foi adicionado corretamente', function() {
-        const usuarios = gerenciarUsuarios();
-        expect(usuarios).to.deep.equal(['Théo', 'Silvana', 'Deivede', 'Maria']);
-    });
-}); 
-
+});
